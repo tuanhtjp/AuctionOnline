f6917f4f492a34464e89b9e2234b5bddb28174ce
Tran Hoang Tuan <tranhoangtuan@hotmail.com>
2016-07-30 14:19:57 +0700

    New project and admin model

---
+++ AuctionOnline/AuctionOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuctionOnline.Startup))]
+namespace AuctionOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
